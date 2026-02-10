0c297b3c94255335191f4eb8085adef7e83b1705
sharryXheisenberg <pandesaurabh4596@gmail.com>
2026-02-10 08:49:13 +0530

    Updated App.jsx file

--- learnwebsockets/client/src/App.jsx
+++ learnwebsockets/client/src/App.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { io } from "socket.io-client";
 
 export const App = () => {
   const _socket = io("http://localhost:3000");
+
+  useEffect(() => {
+    _socket.on("connect", () => {
+      console.log("connected", _socket.id);
+    });
+    _socket.on("welcome", (s) => {
+      console.log(s);
+    });
+
+    return () => {
+      _socket.disconnet();
+    };
+  }, []);
+
   return <div>App</div>;
 };
 
